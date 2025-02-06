767cedffd729df28c2b83c741306dacac3d23317
Phan Minh Cuong <levelup379452850@gmail.com>
2025-02-06 20:15:14 +0700

    The first commit!

---
+++ components/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home(): React.ReactElement {
+  return (
+    <div className="flex flex-row justify-center items-center text-blue-300">
+      <h2 className="text-[36px] mt-[3rem]">Home</h2>
+    </div>
+  );
+}
